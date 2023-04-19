be10eefcec047951658d228c36d2c85a23238a8a
Luan <luaninfo09@gmail.com>
2023-04-19 13:38:11 -0300

    Questao 4

---
+++ Questao4.py
@@ -0,0 +1,8 @@
+num = int(input("Insira o numero => "))
+
+if num > 0:
+    print("{} é positivo".format(num))
+elif num < 0:
+    print("{} é negativo".format(num))
+else:
+    print("{} é neutro".format(num))
